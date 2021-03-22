1af34dbcd2f72fa51755b73737344b28c0dae9f9
Zelechos <zelechos@hotmail.com>
2021-03-22 09:00:58 -0400

    v0.0.3

--- Js/main.js
+++ Js/main.js
@@ -4,6 +4,7 @@ window.addEventListener('load', ()=>{
 
     let Tabla = document.querySelector("#Tabla");
     let Tabla1 = document.querySelector("#Tabla1");
+    let Tabla2 = document.querySelector("#Tabla2");
 
     let Contenido = `
     <h3 class="subtitle">1er.SEMESTRE</h3>
@@ -88,8 +89,6 @@ window.addEventListener('load', ()=>{
             </tr>
     `;
 
-    const Semestres = ["1er.SEMESTRE", "2do.SEMESTRE"];
-    //Creamos la etiqueta del subtitulo h3
     
     let Etiqueta_FileData = function (){
         let list = [];
@@ -131,20 +130,64 @@ window.addEventListener('load', ()=>{
 
 
 //TRAER DATO JSON PARA INTRODUCIRLOS EN LOS TD S            
-    let Data = function (){
-        let list = [];
+    let Data = function (num){
+        let list = [], text;
         // AQUI MISMO PUEDO CREAR LOS ID DE LAS ETIQUETAS TR
         let TR = document.createElement("tr");
         TR.className = "data"
-
+        TR.id = num;
+        let index = num-1;
         let input = document.createElement("input");
         input.type = "checkbox";
         input.name = " ";
         input.id = " ";
-
+        //let URLS = [,'./Js/2do.SEMESTRE.json'];
         for(let i=0;i<11;i++){
             list[i]= document.createElement("td");
         }
+
+        $.get('./Js/Data.json', response =>{
+            //Dado que el valor de el json es un array empieza del indice 0 por eso index = 0
+                if(Number(TR.id) == response[index].N){
+                        for(let i = 0 ; i<list.length ; i++){
+
+                        if(i==0){
+                            text = document.createTextNode(response[index].N);
+                            list[i].appendChild(text);
+                        }else if(i == 1){
+                            text = document.createTextNode(response[index].SIGLA);
+                            list[i].appendChild(text);
+                        }else if(i == 2){
+                            text = document.createTextNode(response[index].ASIGNATURA);
+                            list[i].appendChild(text);
+                        }else if(i == 3){
+                            text = document.createTextNode(response[index].HT);
+                            list[i].appendChild(text);
+                        }else if(i == 4){
+                            text = document.createTextNode(response[index].HP);
+                            list[i].appendChild(text);
+                        }else if(i == 5){
+                            text = document.createTextNode(response[index].HL);
+                            list[i].appendChild(text);
+                        }else if(i == 6){
+                            text = document.createTextNode(response[index].HI);
+                            list[i].appendChild(text);
+                        }else if(i == 7){
+                            text = document.createTextNode(response[index].HEI);
+                            list[i].appendChild(text);
+                        }else if(i == 8){
+                            text = document.createTextNode(response[index].TH);
+                            list[i].appendChild(text);
+                        }else if(i == 9){
+                            text = document.createTextNode(response[index].REQUISITOS);
+                            list[i].appendChild(text);
+                        }else if(i == 10){
+                            list[i].appendChild(input);
+                        }
+                    }   
+                }
+            
+            });
 
         list.forEach(element=> {
             TR.appendChild(element);
@@ -153,59 +196,13 @@ window.addEventListener('load', ()=>{
         console.log(TR);
         return TR;
     }
-
-    function DataSet(){
-        
-        console.log(Etiqueta);
-        $.get('./Js/Data.json', response =>{
-            console.log(response);
-            //for (let x = 0 ; x < response.length; x++){
-
-                for(let i = 0 ; i<list.length ; i++){
-
-                    if(i==0){
-                        text = document.createTextNode(response[x].N);
-                        list[i].appendChild(text);
-                    }else if(i == 1){
-                        text = document.createTextNode(response[x].SIGLA);
-                        list[i].appendChild(text);
-                    }else if(i == 2){
-                        text = document.createTextNode(response[x].ASIGNATURA);
-                        list[i].appendChild(text);
-                    }else if(i == 3){
-                        text = document.createTextNode(response[x].HT);
-                        list[i].appendChild(text);
-                    }else if(i == 4){
-                        text = document.createTextNode(response[x].HP);
-                        list[i].appendChild(text);
-                    }else if(i == 5){
-                        text = document.createTextNode(response[x].HL);
-                        list[i].appendChild(text);
-                    }else if(i == 6){
-                        text = document.createTextNode(response[x].HI);
-                        list[i].appendChild(text);
-                    }else if(i == 7){
-                        text = document.createTextNode(response[x].HEI);
-                        list[i].appendChild(text);
-                    }else if(i == 8){
-                        text = document.createTextNode(response[x].TH);
-                        list[i].appendChild(text);
-                    }else if(i == 9){
-                        text = document.createTextNode(response[x].REQUISITOS);
-                        list[i].appendChild(text);
-                    }else if(i == 10){
-                        list[i].appendChild(input);
-                    }
-                }
-    });
-    }
     
-    
-    function DataForm(Tabla, Cantidad){
+    //Funcion para crear la cantidad de filas para una trabla dinamicamente
+    function DataForm(Tabla, Cantidad,n){
         let init = 0 , etiqueta;
-        let num = 1;
+        let num = n;
         while(init < Cantidad){
-            etiqueta = Data();
+            etiqueta = Data(num);
             etiqueta.id = num;
             Tabla.appendChild(etiqueta);
             init++;
@@ -213,16 +210,15 @@ window.addEventListener('load', ()=>{
         }
     }
 
-
-
-
     function Main(){
         let Tablas_List = [];
-
+        
         Tabla.appendChild(Etiqueta_FileData());
-        DataForm(Tabla, 5);
+        DataForm(Tabla, 5 , 1);
         Tabla1.appendChild(Etiqueta_FileData());
-        DataForm(Tabla1, 10);
+        DataForm(Tabla1, 6 , 6);
+        Tabla2.appendChild(Etiqueta_FileData());
+        DataForm(Tabla2, 6 , 12);
     }
     
     Main();
