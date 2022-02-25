546a0211fa64ea26b057b059a4bd62af2ac7c437
Zelechos <zelechos@hotmail.com>
2022-02-24 23:58:28 -0400

    v0.2.1

--- Js/tables.js
+++ Js/tables.js
@@ -6,10 +6,8 @@ export default class Tables{
     }
 
     print(){
-
         let content = ``;
         for (let index = 0; index < 10; index++) {
-
             (index === 0)
             ? content += `<h1 class="title">INGENIERIA EN CIENCIAS DE LA COMPUTACION</h1>
 <h3 class="subtitle" id="${index}">SEMESTRE 1</h3>
@@ -17,7 +15,6 @@ export default class Tables{
             : content += `<h3 class="subtitle" id="${index}">SEMESTRE ${index+1}</h3>
             <table class="table-style" id="Tabla${index}"></table>\n`
         }
-        console.log(content);
         this.container.innerHTML = content;
     }
 }
